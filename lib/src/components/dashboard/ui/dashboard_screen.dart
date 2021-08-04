1321b16ef56fe78f391646890a86ca1c8c847065
Achin <achin>
2021-08-04 10:46:31 +0530

    service location module and api

---
+++ lib/src/components/dashboard/ui/dashboard_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  DashboardScreen({Key key}) : super(key: key);
+
+  @override
+  _DashboardScreenState createState() {
+    return _DashboardScreenState();
+  }
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
