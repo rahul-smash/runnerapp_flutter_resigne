0e308c9034f37bbf45709f56a5dd030a67a6f5d7
rahul <rahul@signitysolutions.in>
2021-11-24 13:42:17 +0530

    better spelling correct

--- lib/src/utils/app_strings.dart
+++ lib/src/utils/app_strings.dart
@@ -72,7 +72,7 @@ const labelBookingOrderCancel = "Are you sure you want to\n cancel this order?";
 const labelCancelConfirm = "Cancel Confirm";
 const labelBatterPerformanceMsg =
     "For better performance please allow us for background permission";
-const labelNeedPermission = "Need Permission for batter performance";
+const labelNeedPermission = "Need Permission for better performance";
 const labelPayout = 'Payout';
 const labelDeposits = 'Deposits';
 const labelDepositHistory = 'Deposit History';
