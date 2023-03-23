e4735b80e302417f245cf32cfdbee5440f92a545
Rahul <rahul@signitysolutions.in>
2023-03-23 13:39:31 +0530

    cornverges_APP

--- lib/src/network/app_network_constants.dart
+++ lib/src/network/app_network_constants.dart
@@ -1,6 +1,6 @@
 class AppNetworkConstants {
   static const baseUrl = 'https://marketplace.restroapp.com';
-  static const baseStoreParam = '/44';
+  static const baseStoreParam = '/59';
   static const baseRouteV2 = '/runner_v3';
   static const productRoute = '/apistore_v1';
 }
